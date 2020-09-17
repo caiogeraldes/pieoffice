a890f29f2cbeddfd0d3fa8cf37c1a59b3e689417
Caio Geraldes <caioaguida@protonmail.com>
2020-09-17 11:35:23 -0300

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, find_packages
 setup(
         name="pie_tk",
         version="1.0",
+        license="MIT",
         author="Caio Geraldes",
         author_email="caiogeraldes@protonmail.com",
         packages=find_packages(),
+        entry_points={
+        'console_scripts': [
+           'pie_tk=pie_tk.__main__:main'
+            ]
+        },
 )

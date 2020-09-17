70a32ae57408d3df266d5e8fd871d49da46af70b
Caio Geraldes <caioaguida@protonmail.com>
2020-09-17 15:33:09 -0300

    Good to go

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name="pieoffice",
-        version="0.6",
+        version="1.0",
         license="MIT",
         author="Caio Geraldes",
         author_email="caiogeraldes@protonmail.com",

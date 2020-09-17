5d626adf843ae86b46f17fb462dea7d07268df20
Caio Geraldes <caioaguida@protonmail.com>
2020-09-17 11:28:32 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="pie_tk",
+        version="1.0",
+        author="Caio Geraldes",
+        author_email="caiogeraldes@protonmail.com",
+        packages=find_packages(),
+)
